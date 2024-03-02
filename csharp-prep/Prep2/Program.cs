84a44ebdbe55191d62ee1157842651bc59336178
artemisiaporto <artemisiaporto@outlook.com>
2024-03-02 10:39:26 -0300

    Core 1 and 2 of Prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        float gradeNum = float.Parse(grade);
+
+        if (gradeNum >= 90)
+        {
+            Console.WriteLine("Your grade is A. ");
+        }
+        else if (gradeNum >= 80)
+        {
+            Console.WriteLine("Your grade is B. ");
+        }
+        else if (gradeNum >= 70)
+        {
+            Console.WriteLine("Your grade is C. ");
+        }
+        else if (gradeNum >= 60)
+        {
+            Console.WriteLine("Your grade is D. ");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (gradeNum >= 70)
+        {
+            Console.WriteLine("Congratulations, you pass the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You didn't pass the class, but there is still hope, try again next year!");
+        }
     }
 }
