30ac30521007a0e1737b663cd07d5fc9ea4a9b6c
artemisiaporto <artemisiaporto@outlook.com>
2024-03-02 10:53:21 -0300

    Core 3 of Prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,35 +7,38 @@ class Program
         Console.Write("What is your grade percentage? ");
         string grade = Console.ReadLine();
         float gradeNum = float.Parse(grade);
+        string letter = "";
 
         if (gradeNum >= 90)
         {
-            Console.WriteLine("Your grade is A. ");
+            letter = "A";
         }
         else if (gradeNum >= 80)
         {
-            Console.WriteLine("Your grade is B. ");
+            letter = "B";
         }
         else if (gradeNum >= 70)
         {
-            Console.WriteLine("Your grade is C. ");
+            letter = "C";
         }
         else if (gradeNum >= 60)
         {
-            Console.WriteLine("Your grade is D. ");
+            letter = "D";
         }
         else
         {
-            Console.WriteLine("Your grade is F");
+            letter = "F";
         }
+
+        Console.WriteLine($"Your grade is {letter}.");
 
         if (gradeNum >= 70)
         {
-            Console.WriteLine("Congratulations, you pass the class!");
+            Console.WriteLine("Congratulations, you passed the class!");
         }
         else 
         {
-            Console.WriteLine("You didn't pass the class, but there is still hope, try again next year!");
+            Console.WriteLine("You didn't pass the class, but there is still hope! Try again next year.");
         }
     }
 }
