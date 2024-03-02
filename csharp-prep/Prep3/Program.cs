c4e4e868059313d5be695149da40fc92c858343d
artemisiaporto <artemisiaporto@outlook.com>
2024-03-02 14:35:39 -0300

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int numberMagic = randomGenerator.Next(1, 100);
+        ///Console.Write("What is the magic number? ");
+        ///int numberMagic = int.Parse(Console.ReadLine());
+
+        int numberGuess;
+
+        do
+        {
+
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine());
+
+            if (numberGuess > numberMagic) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (numberGuess == numberMagic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("Higher");            
+            }
+        } while (numberGuess != numberMagic);
     }
 }
