7b2c03f2136312a9df5eb423f9468b5386e14d22
artemisiaporto <artemisiaporto@outlook.com>
2024-03-06 17:43:55 -0300

    W02 Learning Activity completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._companyName = "EEG";
+        job1._jobTitle = "Geophysicist";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        ///Console.WriteLine(job1._companyName);
+
+        Job job2 = new Job();
+        job2._companyName = "Oracle";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2030;
+        job2._endYear = 2050;
+
+        ///Console.WriteLine(job2._companyName);
+
+        ///job1.DisplayDetails();
+        ///job2.DisplayDetails();
+
+        Resume resume = new Resume();
+        resume._personName = "Artemísia Jacob";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        ///Console.WriteLine(resume._jobs[0]._jobTitle);
+
+        resume.DisplayDetails();
     }
 }
