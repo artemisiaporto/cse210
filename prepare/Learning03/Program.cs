5f028d2a813b75d9ea328f81c83fb3d231605f4a
artemisiaporto <artemisiaporto@outlook.com>
2024-03-11 22:38:12 -0300

    W03 Learning Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction stFraction = new Fraction();
+        //stFraction.GetTop();
+        //stFraction.GetBottom();
+        Console.WriteLine(stFraction.GetFractionString());
+        Console.WriteLine(stFraction.GetDecimalValue());
+
+        Fraction ndFraction = new Fraction(5);
+        //ndFraction.SetTop(6);
+        //ndFraction.GetTop();
+        //ndFraction.GetBottom();
+        Console.WriteLine(ndFraction.GetFractionString());
+        Console.WriteLine(ndFraction.GetDecimalValue());
+
+        Fraction rdFraction = new Fraction(3,4);
+        //rdFraction.SetTop(6);
+        //rdFraction.GetTop();
+        //rdFraction.SetBottom(7);
+        //rdFraction.GetBottom();
+        Console.WriteLine(rdFraction.GetFractionString());
+        Console.WriteLine(rdFraction.GetDecimalValue());
+
+        Fraction thFraction = new Fraction(1,3);
+        //rdFraction.SetTop(6);
+        //rdFraction.GetTop();
+        //rdFraction.SetBottom(7);
+        //rdFraction.GetBottom();
+        Console.WriteLine(thFraction.GetFractionString());
+        Console.WriteLine(thFraction.GetDecimalValue());
     }
 }
